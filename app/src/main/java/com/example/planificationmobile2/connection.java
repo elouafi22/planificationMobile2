84a7052b535c7b063294684d41e6cdad2089f2be
elouafi22 <elouafimed2@gmail.com>
2023-04-05 18:50:07 +0000

    modification connection java

--- app/src/main/java/com/example/planificationmobile2/connection.java
+++ app/src/main/java/com/example/planificationmobile2/connection.java
@@ -3,6 +3,7 @@ package com.example.planificationmobile2;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -16,9 +17,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class connection {
-   // String url1="http://20.55.44.15:5000/materiel"; pour le serveur
-   private static String urlmed="http://192.168.1.105:5000/authentification";
-   /// private static String url = "http://20.55.44.15:5000/authentification";
+    //private static String url = "http://100.71.33.221:5000/authentification";
+
+    private static String url = "http://100.71.33.221:5000/authentification";
 
     public static void get_connection(Context context, String username ,String password){
         JSONObject postData = new JSONObject();
@@ -30,27 +31,36 @@ public class connection {
             throw new RuntimeException(e);
         }
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST,urlmed, postData,
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, postData,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            String status = response.getString("status");
-                            if (status.equals("success")) {
+                            //String status = response.getString("status");
+                            String session_key = response.getString("session_id");
+                            if (!session_key.isEmpty()) {
                                 // Connexion réussie
-                                Toast.makeText(context, "Connexion réussie", Toast.LENGTH_SHORT).show();
-                                // stoker les inforamtion de l'utilisateur
+                                Toast.makeText(context, "Connexion réussie  la valeur de la session est "+session_key, Toast.LENGTH_SHORT).show();
+                                // Stocker les informations de l'utilisateur
                                 user u1= user.gestInstance();
                                 u1.setNom(username);
                                 u1.setPassword(password);
 
-                                //changer l'activite
+                                // Récupérer la clé de session et la stocker dans les SharedPreferences
+
+                                SharedPreferences sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("session_key", session_key);
+                                editor.apply();
+
+                                // Changer d'activité
                                 Intent nouveau = new Intent(context.getApplicationContext(),home.class);
                                 context.startActivity(nouveau);
                                 ((Activity)context).finish();
                             } else {
                                 // Connexion échouée
                                 String message = response.getString("message");
+                                System.out.println(message);
                                 Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
@@ -66,7 +76,6 @@ public class connection {
         });
         RequestQueue requestQueue = Volley.newRequestQueue(context);
         requestQueue.add(jsonObjectRequest);
-
     }
-
 }
+
--- app/src/main/java/com/example/planificationmobile2/traitementPourProjet/afficherProjet.java
+++ app/src/main/java/com/example/planificationmobile2/traitementPourProjet/afficherProjet.java
@@ -3,6 +3,7 @@ package com.example.planificationmobile2.traitementPourProjet;
 import android.content.Context;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -31,13 +32,33 @@ public class afficherProjet {
 
     public void RecuperLesDonneJson(Context context, String url){
 
-        JsonArrayRequest jonrequest =new JsonArrayRequest(Request.Method.POST, url, null, new Response.Listener<JSONArray>() {
+        // recuperer la valeur de la cle et onvoyee dans la requete
+
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String session_key = sharedPreferences.getString("session_key", "");
+        System.out.println(session_key);
+        JSONArray donnerenvoyer= new JSONArray();
+        JSONObject postData = new JSONObject();
+        try {
+            postData.put("session_id",session_key);
+            donnerenvoyer.put(postData);
+        } catch (
+                JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        /// envoier la requete vers le serveur
+
+        JsonArrayRequest jonrequest =new JsonArrayRequest(Request.Method.POST, url,donnerenvoyer, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
                 for(int i=0;i<response.length();i++){
                     try {
                         JSONObject obj = response.getJSONObject(i);
-                        ensembleProjet.add(new projet(obj.getString("idprojet"),obj.getString("nomprojet"),obj.getString("description"),obj.getString("dateDebut"),obj.getString("chefProjet"),obj.getString("etatProjet")));
+                        //  ensembleProjet.add(new projet(obj.getString("idprojet"),obj.getString("nomprojet"),obj.getString("description"),obj.getString("dateDebut"),obj.getString("chefProjet"),obj.getString("etatProjet")));
+                        ensembleProjet.add(new projet(obj.getString("IDPROJ"),obj.getString("NOMPROJ"),obj.getString("DATEDEB"),obj.getString("DESCRIPTION"),obj.getString("PN"),obj.getString("ETATPROJ")));
                     }catch (JSONException e){
                         e.printStackTrace();
                     }
@@ -69,3 +90,4 @@ public class afficherProjet {
     }
 
 }
+
