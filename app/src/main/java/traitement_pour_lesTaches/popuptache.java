909b0f021f2a2805e5e8e37a69389ce5f0e99fb1
elouafi22 <elouafimed2@gmail.com>
2023-06-03 16:32:14 +0100

    add popup for tache

--- app/src/main/java/traitement_pour_lesTaches/popuptache.java
+++ app/src/main/java/traitement_pour_lesTaches/popuptache.java
@@ -17,6 +17,10 @@ public class popuptache extends Dialog {
     private TextView chef;
     private  TextView nomprojet;
     private TextView dateFin;
+    private TextView nomdatefintache;
+    private TextView nomemp;
+    private TextView nomchef;
+    private TextView Nomprojetques;
     private Button butonok;
 
     public popuptache(Context activity){
@@ -28,6 +32,14 @@ public class popuptache extends Dialog {
         this.emp=findViewById(R.id.lespersonnetachevaleur);
         this.chef=findViewById(R.id.valeurchefprojettache);
         this.nomprojet=findViewById(R.id.valeurnomprojettache);
+
+        this.nomdatefintache = findViewById(R.id.datefintachevaleur);
+        this.nomemp=findViewById(R.id.personnetachequestion);
+        this.nomchef=findViewById(R.id.nomcheftache);
+        this.Nomprojetques=findViewById(R.id.nomprojetdetache);
+
+
+
 
 
     }
@@ -41,6 +53,12 @@ public class popuptache extends Dialog {
         setDateFin(""+dateFin);
         setEmp(emp);
         setChef(chef);
+
+        this.nomdatefintache.setText("Date fin tache :");
+        this.nomemp.setText("EMP :");
+        this.nomchef.setText("Chef :");
+        this.Nomprojetques.setText("Nom projet :");
+
     }
 
     public void setNomtache(String nomtache){ this.nomtache.setText(nomtache);}
